4e1347cf397ac78a25331a3753ce209597812086
Brian Spendolini <brian.spendolini@oracle.com>
2021-06-14 12:28:46 -1000

    ordsoci1

--- developer-library/ords-in-oci/csv-function/function-files/func_secrets.py
+++ developer-library/ords-in-oci/csv-function/function-files/func_secrets.py
@@ -6,30 +6,48 @@
 #
 # Oracle Database Development Tools Group
 # 
+# Requires the following permission added to the policy:
+# Allow dynamic-group functionsDynamicGroup to use secret-family in tenancy
+#
 
 import io
 import json
 import oci
 import csv
 import requests
+import base64
+import sys
 
 from fdk import response
 
+def read_secret_value(signer, secret_id):
+    
+    secret_client = oci.secrets.SecretsClient(config={}, signer=signer)
+    
+    response = secret_client.get_secret_bundle(secret_id)
 
-def csv_insert(ordsbaseurl, schema, dbuser, dbpwd, document):
-    auth=(dbuser, dbpwd)
+    base64_Secret_content = response.data.secret_bundle_content.content
+    base64_secret_bytes = base64_Secret_content.encode('ascii')
+    base64_message_bytes = base64.b64decode(base64_secret_bytes)
+    secret_content = base64_message_bytes.decode('ascii')
+
+    return secret_content
+
+
+def csv_insert(ordsbaseurl, schema, dbuser, secret_ocid, document):
+    auth=(dbuser, secret_ocid)
     batchurl = ordsbaseurl + schema + '/region/batchload?batchRows=5000&errorsMax=20'
     headers = {'Content-Type': 'text/csv'}
     r = requests.post(batchurl, auth=auth, headers=headers, data=document.data.text)
 
-def load_data(signer, namespace, bucket_name, object_name, ordsbaseurl, schema, dbuser, dbpwd):
+def load_data(signer, namespace, bucket_name, object_name, ordsbaseurl, schema, dbuser, secret_ocid):
     client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
     try:
         print("INFO - About to read object {0} in bucket {1}...".format(object_name, bucket_name), flush=True)
         # we assume the file can fit in memory, otherwise we have to use the "range" argument and loop through the file
         csvdata = client.get_object(namespace, bucket_name, object_name)
         if csvdata.status == 200:
-             insert_status = csv_insert(ordsbaseurl, schema, dbuser, dbpwd, csvdata)
+             insert_status = csv_insert(ordsbaseurl, schema, dbuser, secret_ocid, csvdata)
         else:
             raise SystemExit("cannot retrieve the object" + str(object_name))
     except Exception as e:
@@ -62,7 +80,7 @@ def move_object(signer, namespace, source_bucket, destination_bucket, object_nam
 
 def handler(ctx, data: io.BytesIO=None):
     signer = oci.auth.signers.get_resource_principals_signer()
-    object_name = bucket_name = namespace = ordsbaseurl = schema = dbuser = dbpwd = ""
+    object_name = bucket_name = namespace = ordsbaseurl = schema = dbuser = secret_ocid = ""
     try:
         cfg = ctx.Config()
         input_bucket = cfg["input_bucket"]
@@ -70,9 +88,9 @@ def handler(ctx, data: io.BytesIO=None):
         ordsbaseurl = cfg["ords_base_url"]
         schema = cfg["db_schema"]
         dbuser = cfg["db_user"]
-        dbpwd = cfg["dbpwd_cipher"]
+        secret_ocid = cfg["secret_ocid"]
     except Exception as e:
-        print('Missing function parameters: bucket_name, ordsbaseurl, schema, dbuser, dbpwd', flush=True)
+        print('Missing function parameters: bucket_name, ordsbaseurl, schema, dbuser, secret_ocid', flush=True)
         raise
     try:
         body = json.loads(data.getvalue())
@@ -87,7 +105,8 @@ def handler(ctx, data: io.BytesIO=None):
     except Exception as e:
         print('ERROR: bad Event!', flush=True)
         raise
-    load_data(signer, namespace, input_bucket, object_name, ordsbaseurl, schema, dbuser, dbpwd)
+    secret_content = read_secret_value(signer, secret_ocid)    
+    load_data(signer, namespace, input_bucket, object_name, ordsbaseurl, schema, dbuser, secret_content)
     move_object(signer, namespace, input_bucket, processed_bucket, object_name)
 
     return response.Response(
